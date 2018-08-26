7492074a3dccdc12b204530ecdb287a5151e319b
Aldosh <alanshalkey@gmail.com>
2018-08-26 12:18:48 -0600

    adding initial files, working on contorller

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUE ("Chicken Bacon Cheese Burger");
+INSERT INTO burgers (burger_name) VALUE ("Double Angus BBQ Burger with Swiss");
+INSERT INTO burgers (burger_name) VALUE ("California Burger");
+SELECT * FROM burgers;
