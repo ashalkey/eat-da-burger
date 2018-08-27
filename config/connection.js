20ca0bf0d504d8aabe62fbe9f9e0ffab3d095284
Aldosh <alanshalkey@gmail.com>
2018-08-26 18:41:32 -0600

    mysql changes for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+
+
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
